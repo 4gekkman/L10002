99f2584a5af9128b889a594591b28d2a52e0a985
4gekkman <4gekkman@gmail.com>
2016-12-19 04:41:06 +0300

    Autosave to

--- layout.blade.php
+++ layout.blade.php
@@ -1,8 +1,8 @@
-<?php/* =================================================////
-////																										 ////
-////                     Шаблон L-пакета				         ////
-////																										 ////
-////=====================================================////
+<?php /* =================================================////
+////																										  ////
+////                     Шаблон L-пакета				          ////
+////																										  ////
+////======================================================////
 //// 			        		 		   ////
 //// 			  Оглавление			 ////
 //// 			         				   ////
@@ -24,7 +24,7 @@
       В2. Подключение JS-скрипта наследника шаблона
 
 
-////==================================================== */?>
+////=====================================================*/ ?>
 
 <!doctype html>
 <html lang="ru">
@@ -48,8 +48,26 @@
 
 <?php /*----------------------------------------->
 <!-- Б. Содержимое BODY html-разметки шаблона  -->
-<!-------------------------------------------*/ ?>
+<!----------------------------------------------->
+
+  1. Фиксированная шапка сайта
+
+
+
+---------------------------------------------*/ ?>
 <body>
+
+  <?php /*---------------------------->
+  <!-- 1. Фиксированная шапка сайта -->
+  <!------------------------------*/ ?>
+  <div class="site-header">
+    123
+  </div>
+
+
+
+
+
 
   <div id="content" class="content">
     @yield('content')
@@ -88,7 +106,51 @@
     <?php /* JS наследника шаблона */ ?>
     @yield('js')
 
+  <?php /*--------------------------------------->
+  <!-- В3. Подключение счётчика Яндекс.Метрики -->
+  <!-----------------------------------------*/ ?>
+  <script type="text/javascript">
+      (function (d, w, c) {
+          (w[c] = w[c] || []).push(function() {
+              try {
+                  w.yaCounter41412929 = new Ya.Metrika({
+                      id:41412929,
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true,
+                      trackHash:true
+                  });
+              } catch(e) { }
+          });
 
+          var n = d.getElementsByTagName("script")[0],
+              s = d.createElement("script"),
+              f = function () { n.parentNode.insertBefore(s, n); };
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+          if (w.opera == "[object Opera]") {
+              d.addEventListener("DOMContentLoaded", f, false);
+          } else { f(); }
+      })(document, window, "yandex_metrika_callbacks");
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/41412929" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+  <?php /*----------------------------------------->
+  <!-- В4. Подключение счётчика Google Analitycs -->
+  <!-------------------------------------------*/ ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-88629903-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 
 
 
